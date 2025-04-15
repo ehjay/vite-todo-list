c31e693dffdb05844a8c0c475f72821b0798b25f
Andrew Johnston <ehjay@users.noreply.github.com>
2025-04-15 14:22:49 -0400

    continue todo list

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { SyntheticEvent, useState } from 'react';
 import './App.css'
 
 enum Status { TODO, IN_PROGRESS, DONE }
@@ -10,21 +10,36 @@ const statusMessages = {
 }
 
 type Item = {
+  id: string
   description: string
   status: Status
 }
 
+const getRandomId = () => {
+  let randomChars = [],
+  count = 0;
+
+  while (count < 20) {
+    const newHexChar = Math.floor(Math.random() * 16).toString(16)
+    randomChars.push(newHexChar)
+    count++
+  }
+
+  return randomChars.join('');
+}
+
 const defaultItems: Item[] = [
-  { description: "Mow the lawn", status: Status.DONE },
-  { description: "Pick up the kids", status: Status.IN_PROGRESS },
-  { description: "Get milk", status: Status.TODO },
+  { id: getRandomId(), description: "Mow the lawn", status: Status.DONE },
+  { id: getRandomId(), description: "Pick up the kids", status: Status.IN_PROGRESS },
+  { id: getRandomId(), description: "Get milk", status: Status.TODO },
 ];
 
 function App() {
-  const [ items, setItems ] = useState<Item[]>(defaultItems);
+  const [items, setItems] = useState<Item[]>(defaultItems);
+  const [newDesc, setNewDesc] = useState<string>("");
 
   const renderItem = (item: Item) => {
-    return (<p>{statusMessages[item.status]}: {item.description}</p>);
+    return (<p key={item.id}>{statusMessages[item.status]}: {item.description}</p>);
   }
 
   const renderList = () => {
@@ -35,8 +50,30 @@ function App() {
     return items.map(renderItem);
   }
 
+  const handleSubmit = (e: SyntheticEvent) => {
+    // prevent page reload
+    e.preventDefault();
+
+    const form = e.target as typeof e.target & {
+      description: { value: string}
+    }
+
+    setItems([
+      { id: getRandomId(), description: form.description.value , status: Status.TODO },
+      ...items
+    ])
+
+    setNewDesc("")
+  }
+
   return (
     <>
+      <form onSubmit={handleSubmit}>
+        <label>New item: </label>
+        <input name="description" value={newDesc} onChange={e => setNewDesc(e.target.value)} />
+        <button type="submit">Add</button>
+      </form>
+
       <div>
         {renderList()}
       </div>
